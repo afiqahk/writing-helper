6545e34708f586f2f2d45c04506672f7bb6487e6
Afiqah <afiqahkmy@gmail.com>
2023-03-03 14:32:13 +0800

    Move functions into classes RawData, ProgressTracker, RepoCrawler

--- __tracker__/repo.py
+++ __tracker__/repo.py
@@ -5,89 +5,75 @@ import re
 import datetime
 import pandas as pd
 from argparse import Namespace
+from __tracker__.tracker import ProgressTracker
 
 CONFIG_FILE = "./__tracker__/tracker.config.json"
 
-def get_diff_between_commits(commit, commit_prev):
-    change_type_to_check = [ "A", "M", "D"]
-    diffs = [ d for d in commit_prev.diff(commit) if d.change_type in change_type_to_check]
-    return diffs
+class RepoCrawler:
+    def __init__(self, repo_dir=""):
+        self.repo_dir = repo_dir if repo_dir else pathlib.Path.cwd()
+        self.repo = Repo(self.repo_dir)
+        self.change_type_to_check = [ "A", "M", "D"]
 
-def get_commits_since(repo, branchname, since_datetime=None):
-    if since_datetime:
-        if not isinstance(since_datetime, str):
-            since_datetime = since_datetime.isoformat()
-        return list(repo.iter_commits(branchname, since=since_datetime, no_merges=True))
-    else:
-        return list(repo.iter_commits(branchname, no_merges=True))
+    def get_diff_between_commits(self, commit, commit_prev):
+        diffs = [ d for d in commit_prev.diff(commit) if d.change_type in self.change_type_to_check]
+        return diffs
 
-def get_diff_wordcount(diff):
-    if diff.a_blob is None: # new file
-        a_wordcount = 0
-    else:
-        a_blob = diff.a_blob.data_stream.read().decode('utf-8')
-        a_wordcount = len(re.findall(r'\w+', a_blob))
-        
-    if diff.b_blob is None: # deleted file
-        b_wordcount = 0
-    else:
-        b_blob = diff.b_blob.data_stream.read().decode('utf-8')
-        b_wordcount = len(re.findall(r'\w+', b_blob))
-    return b_wordcount - a_wordcount
+    def get_commits_since(self, branchname, since_datetime=None):
+        repo = self.repo
+        if since_datetime:
+            if not isinstance(since_datetime, str):
+                since_datetime = since_datetime.isoformat()
+            return list(repo.iter_commits(branchname, since=since_datetime, no_merges=True))
+        else:
+            return list(repo.iter_commits(branchname, no_merges=True))
 
-def get_branches(repo, excludes=[]):
-    return [ b for b in repo.branches if b.name not in excludes ]
+    def get_diff_wordcount(self, diff):
+        if diff.a_blob is None: # new file
+            a_wordcount = 0
+        else:
+            a_blob = diff.a_blob.data_stream.read().decode('utf-8')
+            a_wordcount = len(re.findall(r'\w+', a_blob))
+            
+        if diff.b_blob is None: # deleted file
+            b_wordcount = 0
+        else:
+            b_blob = diff.b_blob.data_stream.read().decode('utf-8')
+            b_wordcount = len(re.findall(r'\w+', b_blob))
+        return b_wordcount - a_wordcount
 
-def get_rawdata(repo, args):
-    data_branch = []
-    data_commit_datetime = []
-    data_commit_hexsha = []
-    data_diff_file = []
-    data_diff_wordcount = []
-    data_diff_is_renamed = []
-    data_diff_is_renamed_file = []
-    data_diff_is_new_file = []
-    data_diff_is_deleted_file = []
-    
-    args.last_checked_datetime_isoformat = "2022-12-21T15:26:04+08:00"
-    # branch = "scara"
-    branches = get_branches(repo, args.exclude_branches)
-    for branch in branches:
-        commits = get_commits_since(repo, branch.name, args.last_checked_datetime_isoformat)
-        for i in range(1, len(commits)):
-            newer_commit = commits[i - 1]
-            older_commit = commits[i]
-            diffs = get_diff_between_commits(newer_commit, older_commit)
-            for diff in diffs:
-                data_branch.append(branch)
-                data_commit_datetime.append(newer_commit.committed_datetime.isoformat())
-                data_commit_hexsha.append(newer_commit.hexsha)
-                data_diff_file.append(diff.a_path)
-                data_diff_wordcount.append(get_diff_wordcount(diff))
-                data_diff_is_renamed.append(diff.renamed)
-                data_diff_is_renamed_file.append(diff.renamed_file)
-                data_diff_is_new_file.append(diff.new_file)
-                data_diff_is_deleted_file.append(diff.deleted_file)
-    df = pd.DataFrame(data={
-        "data_branch": data_branch,
-        "data_commit_datetime": data_commit_datetime,
-        "data_commit_hexsha": data_commit_hexsha,
-        "data_diff_file": data_diff_file,
-        "data_diff_wordcount": data_diff_wordcount,
-        "data_diff_is_renamed": data_diff_is_renamed,
-        "data_diff_is_renamed_file": data_diff_is_renamed_file,
-        "data_diff_is_new_file": data_diff_is_new_file,
-        "data_diff_is_deleted_file": data_diff_is_deleted_file,
-    })
-    assert True
-    return
+    def get_branches(self, excludes=[]):
+        return [ b for b in self.repo.branches if b.name not in excludes ]
+
+    def get_rawdata(self, args):
+        prg = ProgressTracker()
+        repo = self.repo
+        args.last_checked_datetime_isoformat = "2022-12-21T15:26:04+08:00"
+        branches = self.get_branches(args.exclude_branches)
+        for branch in branches:
+            commits = self.get_commits_since(branch.name, args.last_checked_datetime_isoformat)
+            for i in range(1, len(commits)):
+                newer_commit = commits[i - 1]
+                older_commit = commits[i]
+                diffs = self.get_diff_between_commits(newer_commit, older_commit)
+                for diff in diffs:
+                    prg.rawdata.branch.append(branch)
+                    prg.rawdata.commit_datetime.append(newer_commit.committed_datetime.isoformat())
+                    prg.rawdata.commit_hexsha.append(newer_commit.hexsha)
+                    prg.rawdata.diff_file.append(diff.a_path)
+                    prg.rawdata.diff_wordcount.append(self.get_diff_wordcount(diff))
+                    prg.rawdata.diff_is_renamed.append(diff.renamed)
+                    prg.rawdata.diff_is_renamed_file.append(diff.renamed_file)
+                    prg.rawdata.diff_is_new_file.append(diff.new_file)
+                    prg.rawdata.diff_is_deleted_file.append(diff.deleted_file)
+        prg.save_raw(args.tracker_rawdata_file)
+        return
 
 def run():
-    rw_dir = pathlib.Path.cwd()
-    repo = Repo(rw_dir)
     with open(CONFIG_FILE) as f:
         config = json.load(f)
     args = Namespace(**config)
-    get_rawdata(repo, args)
+    rc = RepoCrawler()
+    rc.get_rawdata(args)
     assert True
     
---
+++ __tracker__/tracker.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+class RawTrackerData:
+    """ Data-only class
+    """
+    def __init__(self):
+        self.branch = []
+        self.commit_datetime = []
+        self.commit_hexsha = []
+        self.diff_file = []
+        self.diff_wordcount = []
+        self.diff_is_renamed = []
+        self.diff_is_renamed_file = []
+        self.diff_is_new_file = []
+        self.diff_is_deleted_file = []
+
+class TrackerData:
+    def __init__(self):
+        self.commit_date = [] #columns
+        self.file = [] #rows
+        self.wordcount = [] #data
+
+class ProgressTracker:
+    def __init__(self):
+        self.rawdata = RawTrackerData()
+    
+    def get_rawdata_column_names(self):
+        return list(vars(self.rawdata).keys())
+    
+    def save_raw(self, path):
+        df = pd.DataFrame(data=vars(self.rawdata))
+        df.to_csv(path, mode='a', header=not is_datafile_empty(path))
+
+def is_datafile_empty(path):
+    try:
+        pd.read_csv(path, index_col=0, nrows=0)
+        return True
+    except pd.errors.EmptyDataError as e:
+        print(f"Empty file '{path}': {e}")
+    except OSError as e:
+        print(f"Unable to open file: {e}")
+    return False
+        
+    
+
+    
+
+    
